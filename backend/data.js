6f405f6d03067024678d292d62872fd526d6d1a7
feifei <francogm98@hotmail.com>
2022-01-13 22:05:56 -0300

    integrated mongodb

--- backend/data.js
+++ backend/data.js
@@ -1,4 +1,19 @@
+import bcrypt from "bcryptjs";
 const data = {
+    users: [
+        {
+            name: "feifei",
+            email: "admin@example.com",
+            password: bcrypt.hashSync("1234", 8),
+            isAdmin: true,
+        },
+        {
+            name: "Jhon",
+            email: "user@example.com",
+            password: bcrypt.hashSync("1234", 8),
+            isAdmin: false,
+        },
+    ],
     products: [
         {
             _id: "1",
